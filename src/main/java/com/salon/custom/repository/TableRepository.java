3207fc578290f5f4eca67f292b0bee0a0b0c2a8a
mrbeonghia <ntnghia199x@gmail.com>
2022-03-23 08:02:04 +0700

    update

--- src/main/java/com/salon/custom/repository/TableRepository.java
+++ src/main/java/com/salon/custom/repository/TableRepository.java
@@ -2,7 +2,6 @@ package com.salon.custom.repository;
 
 import com.salon.base.core.BaseRepository;
 import com.salon.custom.entities.TableEntity;
-import javafx.scene.control.Tab;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
